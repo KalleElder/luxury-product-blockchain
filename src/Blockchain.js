5d72568f1c4625c4ee76770e5248e5dad82c2003
KalleElder <kalle.elder.wikstrom@gmail.com>
2026-08-31 17:56:58 +0200

    feat: implementera Proof-of-Work mining

--- src/Blockchain.js
+++ src/Blockchain.js
@@ -1,6 +1,10 @@
 import crypto from 'crypto'
 
 class Blockchain {
+  constructor() {
+    this.difficulty = process.env.NODE_ENV === 'test' ? 1 : 2
+  }
+
   calculateHash(index, previousHash, transactions, nonce) {
     const data = index + previousHash + JSON.stringify(transactions) + nonce
 
@@ -8,6 +12,23 @@ class Blockchain {
       .createHash('sha256')
       .update(data)
       .digest('hex')
+  }
+
+  mineBlock(block) {
+    const target = '0'.repeat(this.difficulty)
+
+    while (!block.hash.startsWith(target)) {
+      block.nonce++
+
+      block.hash = this.calculateHash(
+        block.index,
+        block.previousHash,
+        block.transactions,
+        block.nonce
+      )
+    }
+
+    return block
   }
 }
 
